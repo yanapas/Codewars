16f4228cd3e40810ea882f25554a82e7993590da
Yana-Pas <monstervan221188@gmail.com>
2020-10-01 12:12:57 -0700

    1

---
+++ 8 kyu Training JS 1 create your first JS function and print Helloworld!.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8
+
+//refer to the example and write your first JS function
+
+function helloWorld() {
+    var str = 'Hello World!';
+    console.log(str);
+    return str;
+}
+console.log(helloWorld());
