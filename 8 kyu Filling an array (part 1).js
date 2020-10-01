ca14580efed501ac355daeeedd47db78bc700dfb
Yana-Pas <monstervan221188@gmail.com>
2020-10-01 11:55:54 -0700

    1

---
+++ 8 kyu Filling an array (part 1).js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/571d42206414b103dc0006a1
+
+function arr(N) {
+    const array = [];
+    for (let i = 0; i <= N-1; i++) {
+        array.push(i);
+
+    }
+    return array;
+}
