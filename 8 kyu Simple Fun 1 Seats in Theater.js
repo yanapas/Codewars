24a82e48e023d98e7a6f093e04679927a295094a
Yana-Pas <monstervan221188@gmail.com>
2020-10-02 17:35:37 -0700

    1

---
+++ 8 kyu Simple Fun 1 Seats in Theater.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/588417e576933b0ec9000045
+
+
+function seatsInTheater(nCols, nRows, col, row) {
+    a1 = nCols - col
+    b1 = nRows - row
+    total1 = a1 * b1
+    return b1 + (total1)
+
+}
