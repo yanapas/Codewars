fe9817058bcef4ad5b0123b0e857d79fa814568a
Yana-Pas <monstervan221188@gmail.com>
2020-10-20 13:41:30 -0700

    1

---
+++ 6 kyu Unique In Order.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/54e6533c92449cc251001667
+
+var uniqueInOrder=function(iterable){
+    let arr = [];
+    for (let i = 0; i < iterable.length; i++){
+
+        if (iterable[i] !== iterable[i + 1]) {
+            arr.push(iterable[i])
+        }
+    }
+    return arr;
+}
