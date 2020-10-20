54692af49f0dd6cd6b881baba02c9eef3f750a62
Yana-Pas <monstervan221188@gmail.com>
2020-10-19 21:34:57 -0700

    1

---
+++ 7 kyu Bumps in the Road.js
@@ -0,0 +1,16 @@
+//https://www.codewars.com/kata/57ed30dde7728215300005fa
+
+function bump(x){
+    let count = 0;
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] === 'n') {
+            count++;
+        }
+    }
+    if (count > 15){
+        return "Car Dead";
+    } else {
+        return "Woohoo!";
+    }
+
+}
