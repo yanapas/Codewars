66f2ca69d427cc3bb441c167098044e02c23f68f
Yana-Pas <monstervan221188@gmail.com>
2020-10-26 15:20:08 -0700

    1

---
+++ 8 kyu Training JS 4 Basic data types--Array.js
@@ -0,0 +1,29 @@
+//https://www.codewars.com/kata/571effabb625ed9b0600107a
+
+function getLength(arr){
+    for (let i = 0; i < arr.length; i++){
+    }
+    return arr.length;
+}
+function getFirst(arr){
+    for (let i = 0; i < arr.length; i++){
+    }
+    return arr[0];
+}
+function getLast(arr){
+    for (let i = 0; i < arr.length; i++){
+    }
+    return arr[arr.length-1];
+}
+function pushElement(arr){
+    var el=1;
+    arr.push(el);
+
+    return arr
+}
+function popElement(arr){
+    const el = 1;
+    arr.pop(el);
+
+    return arr;
+}
