d15dc07167cd66f1cf575d0ca15975eeb11a9e12
Yana-Pas <monstervan221188@gmail.com>
2020-11-07 12:40:18 -0800

    1

---
+++ 8 kyu Determine offspring sex based on genes XX and XY chromosomes.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/56530b444e831334c0000020
+
+function chromosomeCheck(sperm) {
+    return (sperm === "XX")? "Congratulations! You're going to have a daughter." : "Congratulations! You're going to have a son."
+}
+
