6ad0e95ae61cf84b35bc088b9d57969762ce228e
Yana-Pas <monstervan221188@gmail.com>
2020-11-07 12:41:27 -0800

    1

---
+++ 8 kyu Grasshopper - Debug.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/55cb854deb36f11f130000e1
+
+function weatherInfo (temp) {
+    var c = convertToCelsius(temp);
+    if (c < 0) return (c + " is freezing temperature");
+    else return (c + " is above freezing temperature");
+}
+
+function convertToCelsius (temperature) {
+    var celsius = (temperature - 32) * (5/9);
+    return celsius;
+}
