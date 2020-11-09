9b4a3d250b93c414fb450bd43384c4bdf46ffd1d
Yana-Pas <monstervan221188@gmail.com>
2020-11-09 09:59:33 -0800

    1

---
+++ 8 kyu Alan Partridge II - Apple Turnover.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/580a094553bd9ec5d800007d
+
+function apple(x){
+    return x ** 2 > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.'
+}
