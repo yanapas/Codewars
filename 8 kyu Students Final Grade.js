b9633ec4c3ad0dc0a0b335d024bb53050029d1f3
Yana-Pas <monstervan221188@gmail.com>
2020-11-11 17:25:17 -0800

    1

---
+++ 8 kyu Students Final Grade.js
@@ -0,0 +1,16 @@
+//https://www.codewars.com/users/IanaP/completed_solutions
+
+function finalGrade (exam, projects) {
+    if (exam > 90 || projects > 10){
+        return 100;
+    }
+    if (exam > 75 && projects >= 5){
+        return 90;
+    }
+    if (exam > 50 && projects >= 2){
+        return 75;
+    } else {
+        return 0;
+    }
+
+}
