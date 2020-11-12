040e72eabe23fc7dbb472446eeaecf23a4d8bceb
Yana-Pas <monstervan221188@gmail.com>
2020-11-11 16:21:06 -0800

    1

---
+++ 8 kyu L1 Set Alarm.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/568dcc3c7f12767a62000038
+
+function setAlarm(employed, vacation){
+    if(employed === true && vacation === false){
+        return true;
+    } else {
+        return false
+    }
+}
