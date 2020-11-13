f63c301d2863384323e75b46abd2d14c3c2c6dff
Yana-Pas <monstervan221188@gmail.com>
2020-11-13 11:15:00 -0800

    1

---
+++ 8 kyu Are You Playing Banjo.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/53af2b8861023f1d88000832
+
+function areYouPlayingBanjo(name) {
+
+    return (name[0] === 'R' || name[0] === 'r')? `${name} plays banjo` : `${name} does not play banjo` ;
+}
