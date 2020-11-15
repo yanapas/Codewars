faaca155de458cddf58bfa7938aad77efaab6034
Yana-Pas <monstervan221188@gmail.com>
2020-11-14 23:39:15 -0800

    2

--- 8 kyu Days in the year.js
+++ 8 kyu Days in the year.js
@@ -1,12 +1,6 @@
 //https://www.codewars.com/kata/56d6c333c9ae3fc32800070f
 
-function isLeap(year) {
-    return (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0));
-}
-
 function yearDays(year)
 {
-    const daysInYear = isLeap(year)? 366: 365;
-
-    return `${year} has ${daysInYear} days`;
+    return (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0))? `${year} has 366 days` : `${year} has 365 days`;
 }
