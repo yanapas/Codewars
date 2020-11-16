ebeb71cf5a7449ecc75079af17401eaf9c2c44a9
Yana-Pas <monstervan221188@gmail.com>
2020-11-15 21:37:30 -0800

    1

---
+++ 8 kyu Reversed sequence.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+
+
+const reverseSeq = n => {
+    let arr = []
+    for (let i = n; i > 0; i-- ){
+        arr.push(i)
+    }
+    return arr;
+};
