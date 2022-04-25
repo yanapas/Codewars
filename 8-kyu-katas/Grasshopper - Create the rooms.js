e697145727f2cef676352c88e0e01b9174be0030
Iana Pastukhova <monstervan221188@gmail.com>
2022-04-25 14:42:20 -0700

    Grasshopper - Create the rooms

---
+++ 8-kyu-katas/Grasshopper - Create the rooms.js
@@ -0,0 +1,19 @@
+//https://www.codewars.com/kata/56a29b237e9e997ff2000048/train/javascript
+
+var rooms = {
+  rooms1: {
+    name: '',
+    description: '',
+    completed: '',
+  },
+  rooms2: {
+    name: '',
+    description: '',
+    completed: '',
+  },
+  rooms3: {
+    name: '',
+    description: '',
+    completed: '',
+  }
+}
